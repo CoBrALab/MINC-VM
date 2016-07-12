6c62063a756bf47e41e22e29a33f1e002f029003
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-07-12 12:56:54 -0400

    Enable virtual filesystem for user

--- virtualaddons.sh
+++ virtualaddons.sh
@@ -12,3 +12,6 @@ mount /dev/sr1 /mnt
 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions
 rm -rf /tmp/VBoxGuestAdditions
 umount /mnt
+
+#Add user to premission to access virtualbox drive
+usermod -a -G vboxsf minc
