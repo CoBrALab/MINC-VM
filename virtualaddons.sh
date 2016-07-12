307d02835fe8b6e9510141d28a525c5a692cf5d0
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-07-12 11:48:40 -0400

    Install pre-reqs for addons

--- virtualaddons.sh
+++ virtualaddons.sh
@@ -2,7 +2,11 @@
 set -eu
 set -x
 
-#Virtualbox addons
+#Prerequisites
+apt update
+apt install -y dkms
+
+#Mount virtualbox CD and install addons
 mkdir -p /mnt
 mount /dev/sr1 /mnt
 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions
