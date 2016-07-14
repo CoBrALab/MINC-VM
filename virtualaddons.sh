797727465e6556476edfcdd3f09cc5e4893413a0
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-07-14 17:30:15 -0400

    Initial VMWare structure

--- virtualaddons.sh
+++ virtualaddons.sh
@@ -4,16 +4,18 @@ set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-#Prerequisites
-apt update
-apt install -y dkms
+if [[ "$PACKER_BUILDER_TYPE" == virtualbox* ]]; then
+  #Prerequisites
+  apt update
+  apt install -y dkms
 
-#Mount virtualbox CD and install addons
-mkdir -p /mnt
-mount /dev/sr1 /mnt
-/mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions
-rm -rf /tmp/VBoxGuestAdditions
-umount /mnt
+  #Mount virtualbox CD and install addons
+  mkdir -p /mnt
+  mount /dev/sr1 /mnt
+  /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions
+  rm -rf /tmp/VBoxGuestAdditions
+  umount /mnt
 
-#Add user to premission to access virtualbox drive
-usermod -a -G vboxsf minc
+  #Add user to premission to access virtualbox drive
+  usermod -a -G vboxsf minc
+fi
