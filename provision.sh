9f7854411d8725c47015bb83e452f5a89493de52
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-07-15 15:53:32 -0400

    Add lsof for RMINC

--- provision.sh
+++ provision.sh
@@ -108,7 +108,7 @@ rm -f itksnap_minc.tar.gz
 apt-get purge $(dpkg -l | tr -s ' ' | cut -d" " -f2 | sed 's/:amd64//g' | grep -e -E '(-dev|-doc)$')
 
 #Install R
-apt install -y --no-install-recommends r-base r-base-dev r-recommended
+apt install -y --no-install-recommends r-base r-base-dev r-recommended lsof
 
 #Install rstudio
 wget --progress=dot:mega $rstudio
