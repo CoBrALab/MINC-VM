8c13a5be5902437b7ab4fb60bd27317d7a86b9f5
Gabriel A. Devenyi <gdevenyi@gmail.com>
2019-04-12 13:55:06 -0400

    Switch to using updated virtualbox guest additions

--- virtualaddons.sh
+++ virtualaddons.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-set -eu
+#set -euo pipefail
 set -x
-
-export DEBIAN_FRONTEND=noninteractive
 
 #Prerequisites
 apt update
-apt install -y virtualbox-guest-dkms virtualbox-guest-x11 virtualbox-guest-utils
+apt install -y build-essential
 
 #Add user to premission to access virtualbox drive
 addgroup --system vboxsf || true
-usermod -a -G vboxsf minc
+usermod -a -G vboxsf minc || true
+
+# Mount the disk image
+cd /tmp
+mkdir /tmp/isomount
+mount -t iso9660 -o loop /home/minc/VBoxGuestAdditions.iso /tmp/isomount
+
+# Install the drivers
+/tmp/isomount/VBoxLinuxAdditions.run
+
+# Cleanup
+umount isomount
+rm -rf isomount /home/minc/VBoxGuestAdditions.iso
